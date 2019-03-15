b1bbafe2690a66f9c85963c63f045f2957b24717
785400320@qq.com <785400320@qq.com>
2019-03-15 22:13:21 +0800

    3333

--- wish/application/api/controller/User.php
+++ wish/application/api/controller/User.php
@@ -142,7 +142,7 @@ class User extends BaseController {
             
             $aMainU = Db::name('users')->find($mUser['user_id']);
             $aUserInfo['nickName'] = $aMainU['username'] ? $aMainU['username'] : $aUserInfo['nickName'];
-            $aUserInfo['mobile'] = $mUser['mobile'] ? $mUser['mobile'] : '';
+            $aUserInfo['mobile'] = $aMainU['mobile'] ? $aMainU['mobile'] : '';
             $aUserInfo['sign'] = $mUser['sign'] ? $mUser['sign'] : '';
             $userId = $mUser['user_id'];
             Db::name('user_open_binds')->where(['user_id' => $userId])->update($aUserOpenBinds);
