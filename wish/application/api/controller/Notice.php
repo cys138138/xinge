c62958a8500c1c1a87a9d80ccb6f72a56288cd38
785400320@qq.com <785400320@qq.com>
2019-03-19 21:58:18 +0800

    87585

--- wish/application/api/controller/Notice.php
+++ wish/application/api/controller/Notice.php
@@ -37,7 +37,7 @@ class Notice extends BaseController {
     public function getGuize(){        
         $aData = Db::name('art_notice')->where(['status' => 1, 'is_deleted' => 0])->find();
         $aData['create_time'] = date('Y-m-d H:i:s',$aData['create_time']);
-        $aData['desc'] = htmlspecialchars_decode($aData['desc']);
+        $aData['content'] = htmlspecialchars_decode($aData['content']);
         return $this->success('获取规则成功', null, $aData);
     }
     
