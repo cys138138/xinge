8b876629021af460803f6a3c0ffbd42af894bbad
785400320@qq.com <785400320@qq.com>
2019-03-24 20:57:12 +0800

    狗日

--- wish/application/api/controller/User.php
+++ wish/application/api/controller/User.php
@@ -432,7 +432,9 @@ class User extends BaseController {
         
         $qrcodeImgUrl = static::getUserQr($aLastWish['uid']);
         
-        $qrcodeImg = imagecreatefromstring(file_get_contents($qrcodeImgUrl));   
+       $qrcodeImg = $this->imgzip($qrcodeImgUrl, 132, 132);
+        
+//        $qrcodeImg = imagecreatefromstring(file_get_contents($qrcodeImgUrl));   
 		$bgImgUrl = APP_PATH.'/../bg3.png';
         $bigImg = imagecreatefrompng($bgImgUrl);
         
@@ -478,6 +480,35 @@ class User extends BaseController {
 		
 		header('Content-type:image/png');   
         imagepng($baseImg);die;             
+    }
+    
+    public function imgzip($src, $newwid, $newhei) {
+        $imgInfo = getimagesize($src);
+        $imgType = image_type_to_extension($imgInfo[2], false);
+
+        //声明图片   打开图片 在内存中
+        $image = imagecreatefromstring(file_get_contents($src));
+        //方便配置长度宽度、高度，设置框为变量wid,高度为hei
+        $wid = $imgInfo[0];
+        $hei = $imgInfo[1];
+        //判断长度和宽度，以方便等比缩放,规格按照500, 320
+        if ($wid > $hei) {
+            $wid = $newwid;
+            $hei = $newwid / ($wid / $hei);
+        } else {
+            $wid = $newhei * ($wid / $hei);
+            $hei = $newhei;
+        }
+        //在内存中建立一张图片
+        $images2 = imagecreatetruecolor($newwid, $newhei); //建立一个500*320的图片
+        //将原图复制到新建图片中
+        //imagecopyresampled($dst_image, $src_image, $dst_x, $dst_y, $src_x, $src_y, $dst_w, $dst_h, $src_w, $src_h)
+
+        imagecopyresampled($images2, $image, 0, 0, 0, 0, $wid, $hei, $imgInfo[0], $imgInfo[1]);
+
+        //销毁原始图片
+        imagedestroy($image);
+        return $images2;
     }
 
     public function yuanjiao($imgpath) {
