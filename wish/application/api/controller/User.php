8195d20396d3ec010ae85678ae3da39333843c41
cys138138 <785400320@qq.com>
2019-03-27 15:03:57 +0800

    89898

--- wish/application/api/controller/User.php
+++ wish/application/api/controller/User.php
@@ -315,7 +315,8 @@ class User extends BaseController {
             return $this->error('愿望实现，开启新的愿望吧');
         }
         $aData = UserService::getWishNeedMoneyInfo($aWish['id']);
-	$aData['wish_id'] = $aWish['id'];
+		$aData['wish_id'] = $aWish['id'];
+		$aData['wish_title'] = $aWish['title'];
         $this->success('获取成功 ：total_day_nums 已经生存天数 total_nums 总共存了多少桶 today_need_money 今天需要存的桶数 back_total_nums 还需要几天返回', '', $aData);
     }
 
@@ -391,6 +392,7 @@ class User extends BaseController {
             $uid = $aLastWish['uid'];
         }
         $aUser = Db::name('users')->where(['id' => $uid])->find();
+        $aData['wish_title'] = $aLastWish['title'];
         $aData['head_img'] = $aUser['head_img_url'];
         $aData['wish_id'] = $wishId;
         $aData['login_date'] = date('Y-m-d', $aLastWish['create_time']);
