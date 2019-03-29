fa0865f074cf59cf830a6b767c5f5268a11e6dbb
cys138138 <785400320@qq.com>
2019-03-29 17:59:25 +0800

    h5登录

---
+++ wish/application/api/controller/Huser.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace app\api\controller;
+
+use think\Db;
+use controller\BasicAdmin;
+
+/**
+ * 艺人管理
+ */
+class Huser extends BasicAdmin {
+
+   /**
+     * 网页授权测试
+     * @return string
+     * @throws \think\Exception
+     * @throws \think\exception\PDOException
+     */
+    public function oauth()
+    {
+        $fans = WechatService::webOauth(1);
+        print_r($fans);       
+    }
+}
