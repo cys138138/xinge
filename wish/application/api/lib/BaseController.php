a531fd371d2a05f2f521ae6266465cd01db083d4
cys138138 <785400320@qq.com>
2019-04-01 16:14:11 +0800

    988

--- wish/application/api/lib/BaseController.php
+++ wish/application/api/lib/BaseController.php
@@ -32,6 +32,7 @@ class BaseController extends Controller
 		header("Access-Control-Allow-Origin:*");
 		header("Access-Control-Allow-Methods:*");
 		header("Access-Control-Allow-Credentials:true");
+		header('Access-Control-Allow-Headers:x-requested-with,content-type');
         return parent::__construct();
     }
 
